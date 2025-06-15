192eeeeeef7ee289373e7586b88e06b884b8ce38
ibmealways <122654069+ibmealways@users.noreply.github.com>
2025-06-15 19:14:24 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-green-100 text-center p-4">
+      <h1 className="text-4xl font-bold text-green-800">Nature’s Elixirz</h1>
+      <p className="text-lg mt-2 text-green-700">Heal N Blend Smoothie Generator</p>
+    </div>
+  );
+}
+
+export default App;
