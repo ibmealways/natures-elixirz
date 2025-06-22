97719616832b61ba9f741afd825b347e5e41b953
ibmealways <122654069+ibmealways@users.noreply.github.com>
2025-06-22 10:03:43 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
+import SmoothieGenerator from "./components/SmoothieGenerator";
+import "./App.css";
 
 function App() {
   return (
-    <div style={{ color: "green", textAlign: "center", paddingTop: "50px" }}>
-      <h1>🌿 Nature’s Elixirz is LIVE!</h1>
-      <p>If you see this, the frontend is working!</p>
+    <div className="app-wrapper">
+      <SmoothieGenerator />
     </div>
   );
 }
 
 export default App;
+
